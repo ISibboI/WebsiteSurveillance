fd99c26effbdfa299d50f529c8d846fed98a3294
Sibbo <isibboi@gmail.com>
2013-11-10 16:30:17 +0100

    First commit, Version 1.0

---
+++ src/de/isibboi/websitesurveillance/WebsiteSurveillance.java
@@ -0,0 +1,161 @@
+package de.isibboi.websitesurveillance;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import javax.swing.JOptionPane;
+import javax.xml.bind.DatatypeConverter;
+
+public class WebsiteSurveillance {
+	private static final String HASH_PROPERTY_PREFIX = "hash_";
+	private static final String URL_PROPERTY_PREFIX = "url_";
+	private static final String SITES_LIST_PROPERTY = "sites";
+	private static final String DEBUG_FLAG = "debug";
+
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+		Properties p = new Properties();
+		File f = getFile();
+		Reader in = new FileReader(f);
+		p.load(in);
+		in.close();
+
+		if (p.getProperty(SITES_LIST_PROPERTY) == null) {
+			p.setProperty(SITES_LIST_PROPERTY, "");
+			error("No sites specified!");
+		}
+		
+		if (p.getProperty(DEBUG_FLAG) != null) {
+			info("Debug flag set");
+		}
+
+		String[] names = p.getProperty(SITES_LIST_PROPERTY).split("\\s*,\\s*");
+		List<String> changed = new ArrayList<>();
+		List<String> added = new ArrayList<>();
+
+		for (String name : names) {
+			if (p.getProperty(URL_PROPERTY_PREFIX + name) == null) {
+				error("Missing url for: " + name);
+			}
+
+			try {
+				URL url = new URL(p.getProperty(URL_PROPERTY_PREFIX + name));
+				String newHash = getHash(url);
+
+				if (p.getProperty(HASH_PROPERTY_PREFIX + name) == null) {
+					added.add(name);
+					p.setProperty(HASH_PROPERTY_PREFIX + name, newHash);
+				} else {
+					if (!newHash.equals(p.getProperty(HASH_PROPERTY_PREFIX + name))) {
+						changed.add(name);
+						p.setProperty(HASH_PROPERTY_PREFIX + name, newHash);
+					}
+				}
+			} catch (MalformedURLException e) {
+				error("Malformed url: " + p.getProperty(URL_PROPERTY_PREFIX + name));
+			}
+		}
+
+		Writer out = new FileWriter(f);
+		p.store(out, "");
+		out.close();
+
+		if (added.size() > 0) {
+			info("Websites added: " + listToString(added));
+		}
+
+		if (changed.size() > 0) {
+			warn("Websites changed: " + listToString(changed));
+		}
+	}
+
+	private static String listToString(List<String> list) {
+		Iterator<String> items = list.iterator();
+		StringBuilder result = new StringBuilder();
+
+		while (items.hasNext()) {
+			result.append(items.next());
+
+			if (items.hasNext()) {
+				result.append(", ");
+			}
+		}
+
+		return result.toString();
+	}
+
+	private static String getHash(URL url) throws IOException, NoSuchAlgorithmException {
+		MessageDigest md5 = MessageDigest.getInstance("MD5");
+		InputStream in = url.openStream();
+		byte[] buffer = new byte[1024 * 64];
+		int length = 0;
+
+		while (true) {
+			length = in.read(buffer);
+
+			if (length == -1) {
+				break;
+			} else {
+				md5.update(buffer, 0, length);
+			}
+		}
+
+		byte[] digest = md5.digest();
+		return DatatypeConverter.printHexBinary(digest);
+	}
+
+	private static File getFile() throws IOException {
+		File home = new File(System.getProperty("user.home"));
+
+		if (!home.isDirectory()) {
+			error("No user home directory found.");
+		}
+
+		File configDir = new File(home, ".websitechangetracker");
+
+		if (configDir.exists() && !configDir.isDirectory()) {
+			error(configDir.getAbsolutePath() + " already exists, but is not a file");
+		}
+
+		if (!configDir.exists()) {
+			configDir.mkdir();
+		}
+
+		File f = new File(configDir, ".properties");
+
+		if (f.exists() && !f.isFile()) {
+			error(".properties already exists, but is not a file");
+		}
+
+		if (!f.exists()) {
+			f.createNewFile();
+		}
+
+		return f;
+	}
+
+	private static void info(String info) {
+		JOptionPane.showMessageDialog(null, info, "Information", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	private static void warn(String warning) {
+		JOptionPane.showMessageDialog(null, warning, "Information", JOptionPane.WARNING_MESSAGE);
+	}
+
+	private static void error(String error) {
+		JOptionPane.showMessageDialog(null, error, "Error!", JOptionPane.ERROR_MESSAGE);
+		System.exit(0);
+	}
+}
